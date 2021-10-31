bd681bf6ee7dd96ea03ad6ae5e9fd8b81d75e24b
unknown <Osni Seara>
2021-10-31 19:24:24 -0300

    Exemplos práticos

---
+++ type-boolean.js
@@ -0,0 +1,9 @@
+const primeiroNumero = 2;
+const segundoNumero = 2;
+
+//console.log(primeiroNumero===segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
